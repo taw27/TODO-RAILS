76af13cafbbe5722faa66457c4315896041cf5b3
Tawshif Alam <tawshif27@gmail.com>
2021-11-06 15:59:24 -0700

    Add create todo functionality

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,20 @@
 class TodosController < ApplicationController
   def index
     @todos = Todo.all
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.create(todo_params)
+
+    @todo.save
+
+    redirect_to action: 'index'
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:todo, :status)
   end
 end
